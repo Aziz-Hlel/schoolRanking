ac10d7e369265bea89f6a36bc7d4fdbeabff1e6a
Mohamed Aziz Ben Hlel <m.aziz.hlel@gmail.com>
2025-11-13 16:17:47 +0100

    hopefully fixed reverse proxy

--- apps/admin/src/components/Forms/DetachedForms/Students/use-Get-Array.ts
+++ apps/admin/src/components/Forms/DetachedForms/Students/use-Get-Array.ts
@@ -1,16 +1,9 @@
-import {
-  useFieldArray,
-  type FieldName,
-  type UseFieldArrayProps,
-  type UseFormReturn,
-} from 'react-hook-form';
+import { useFieldArray, type UseFieldArrayProps, type UseFormReturn } from 'react-hook-form';
 
 interface DetachedFormProps<T> {
   form: UseFormReturn<T>;
   fieldName: UseFieldArrayProps<T>['name'];
 }
-
-type FieldType<T, K extends keyof T> = T[K];
 
 export const useGetArray = <T>({ form, fieldName }: DetachedFormProps<T>) => {
   const { fields, append, remove, swap } = useFieldArray({
@@ -21,17 +14,12 @@ export const useGetArray = <T>({ form, fieldName }: DetachedFormProps<T>) => {
   const feesLength = fields.length;
 
   const handleDelete = (index: number) => {
-    // const item = fees[index];
-    // const itemIndex = feeItemsFields.findIndex((feeItem) => feeItem.id === item.id)
     remove(index);
   };
 
   const handleMove = (oldIndex: number, newIndex: number) => {
     swap(oldIndex, newIndex);
   };
-
-  // type FieldType2 = FieldType<T, fieldName>;
-  const append2 = (object: UseFormReturn<T>[]) => {};
 
   return {
     fields,
