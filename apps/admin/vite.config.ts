a5af68ba29f6fb62afebbe3c9510493ed17cb2b0
Tigana <m.aziz.hlel@gmail.com>
2026-03-17 10:49:08 +0100

    minor improv

--- apps/admin/vite.config.ts
+++ apps/admin/vite.config.ts
@@ -1,34 +1,36 @@
+import { defineConfig, loadEnv } from 'vite';
 import path from 'path';
 import tailwindcss from '@tailwindcss/vite';
-import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-function getPort(): number | undefined {
-  const NODE_ENV = process.env.VITE_NODE_ENV;
-  if (!NODE_ENV) throw new Error(`❌ Missing required environment variable: NODE_ENV`);
+function getPort(mode: string): number | undefined {
+  const env = loadEnv(mode, process.cwd());
 
-  const value = process.env.VITE_ADMIN_PORT;
+  const NODE_ENV = env.VITE_NODE_ENV;
+  if (!NODE_ENV) throw new Error(`❌ Missing required environment variable: VITE_NODE_ENV`);
 
-  if (!value && ['development', 'test'].includes(NODE_ENV))
-    throw new Error(`❌ Missing required VITE_ADMIN_PORT when NODE_ENV is ${NODE_ENV}`);
+  const value = env.VITE_ADMIN_PORT;
+
+  if (!value && ['dev', 'build'].includes(NODE_ENV))
+    throw new Error(`❌ Missing required VITE_ADMIN_PORT when VITE_NODE_ENV is ${NODE_ENV}`);
   if (value && isNaN(Number(value)))
     throw new Error(`❌ Invalid value for VITE_ADMIN_PORT: "${value}" is not a number`);
 
   return Number(value) || undefined;
 }
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
+export default ({ mode }: { mode: string }) =>
+  defineConfig({
+    plugins: [react(), tailwindcss()],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+      },
     },
-  },
 
-  server: {
-    port: getPort(),
-    host: '0.0.0.0', // allow external access (needed in Docker)
-    allowedHosts: ['*'],
-  },
-});
+    server: {
+      port: getPort(mode),
+      host: '0.0.0.0', // allow external access (needed in Docker)
+      allowedHosts: ['*'],
+    },
+  });
